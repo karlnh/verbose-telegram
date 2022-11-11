0ae5a45d50aed3939eb72986783703d61be24db5
karlnh <hubigkarl@gmail.com>
2022-11-11 00:57:47 -0600

    add basic html; add basic css

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// When I click start button,
+// Then a timer starts,
+// Then a question is presented.
+
+// When a question is answered,
+// Then another question is presented.
+
+// When a question is answered incorrectly,
+// Then time is subtracted from the clock.
+
+// When all questions are answered OR
+// When the timer reaches 0
+// Then the game is over
+
+// When the game is over
+// Then score is displayed,
+// Then initials can be entered,
+// Then score and initials can be saved.
